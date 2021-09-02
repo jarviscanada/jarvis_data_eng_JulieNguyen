341b7868ec6c8b29a8eea218e72e2d424a44c583
Julie Nguyen <julie6.nguyen@gmail.com>
2021-09-02 22:37:50 +0000

    psql docker script

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -1,3 +1,47 @@
 #!/bin/bash
 
-echo "Hello World
+cmd=$1
+db_username=$2
+db_password=$3
+
+#check if docker is running
+if (! docker stats --no-stream ); then
+  sudo systemctl status docker || systemctl start docker
+fi
+
+if [ "${cmd}" == "create" ]; then
+  if [ "$(docker container ls -a -f name=jrvs-psql | wc -l)" == 2 ]; then
+    echo "Error: Docker container \"jrvs-psql\" already exists."
+    exit 1
+  fi
+
+  if [ "$#" != 3 ]; then
+    echo "Error: Missing username or password."
+    exit 1
+  fi
+
+  docker volume create pgdata
+  docker run --name jrvs-psql -e POSTGRES_PASSWORD="${db_password}" -e POSTGRES_USER="${db_username}" -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres
+  exit $?
+fi
+
+if [ "$(docker container ls -a -f name=jrvs-psql | wc -l)" != 2 ]; then
+  echo "Error: Docker container \"jrvs-psql\" does not exist."
+  exit 1
+fi
+
+if [ "${cmd}" == "start" ]; then
+  #start container
+  docker container start jrvs-psql
+  exit $?
+fi
+
+if [ "${cmd}" == "stop" ]; then
+  #stop container
+  docker container stop jrvs-psql
+  exit $?
+fi
+
+echo "Error: Invalid command."
+
+exit 0
