871114b0c7528081d1226162f62d925db6ef30b6
Julie Nguyen <julie6.nguyen@gmail.com>
2021-09-03 00:28:40 +0000

    psql docker script

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#three command line arguments: cmd, db_username, db_password
 cmd=$1
 db_username=$2
 db_password=$3
@@ -9,39 +10,47 @@ if (! docker stats --no-stream ); then
   sudo systemctl status docker || systemctl start docker
 fi
 
+#if command is to create a docker container
 if [ "${cmd}" == "create" ]; then
+  #if docker container already exists, error
   if [ "$(docker container ls -a -f name=jrvs-psql | wc -l)" == 2 ]; then
     echo "Error: Docker container \"jrvs-psql\" already exists."
     exit 1
   fi
 
+  #if username or password is not in the command line, error
   if [ "$#" != 3 ]; then
     echo "Error: Missing username or password."
     exit 1
   fi
 
+  #create docker volume & docker container "jrvs-psql"
   docker volume create pgdata
   docker run --name jrvs-psql -e POSTGRES_PASSWORD="${db_password}" -e POSTGRES_USER="${db_username}" -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres
   exit $?
 fi
 
+#if docker container has not been created, error
 if [ "$(docker container ls -a -f name=jrvs-psql | wc -l)" != 2 ]; then
   echo "Error: Docker container \"jrvs-psql\" does not exist."
   exit 1
 fi
 
+#if command is start, then start docker container
 if [ "${cmd}" == "start" ]; then
   #start container
   docker container start jrvs-psql
   exit $?
 fi
 
+#if command is stop, then stop docker container
 if [ "${cmd}" == "stop" ]; then
   #stop container
   docker container stop jrvs-psql
   exit $?
 fi
 
+#if command does not exist
 echo "Error: Invalid command."
 
 exit 0
