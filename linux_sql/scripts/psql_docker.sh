69bc10339c75542bb81e602485fd139cdb1c872f
Julie Nguyen <julie6.nguyen@gmail.com>
2021-09-07 16:10:06 +0000

    psql_docker.sh code review

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -11,37 +11,32 @@ container_wc=$(docker container ls -a -f name=jrvs-psql | wc -l)
 #check if docker is running, if true, start docker.
 sudo systemctl status docker >/dev/null 2>/dev/null || systemctl start docker
 
-#if command is "create", create a docker container
-if [ "${cmd}" == "create" ]; then
+case "${cmd}" in
+  #if command is create, then create docker container
+  "create" )
     if [ "$container_wc" == 2 ]; then
-      echo "Error: Docker container \"jrvs-psql\" already exists."
-      exit 1
-    #if username or password is not in the command line (command line does not have 3 arguments), error
-    elif [ "$#" != 3 ]; then
-      echo "Error: Missing username or password."
+          echo "Error: Docker container \"jrvs-psql\" already exists."
+          exit 1
+        #if username or password is not in the command line (command line does not have 3 arguments), error
+        elif [ "$#" != 3 ]; then
+          echo "Error: Missing username or password."
+          exit 1
+        fi
+
+        #create docker volume & docker container "jrvs-psql"
+        docker volume create pgdata
+        docker run --name jrvs-psql -e POSTGRES_PASSWORD="${db_password}" -e POSTGRES_USER="${db_username}" -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres
+        exit $?
+  ;;
+  #if command is start/stop, then start/stop docker container
+  "start" | "stop")
+    #if docker container has not been created, error
+    if [ "$container_wc" != 2 ]; then
+      echo "Error: Docker container \"jrvs-psql\" does not exist."
       exit 1
     fi
 
-    #create docker volume & docker container "jrvs-psql"
-    docker volume create pgdata
-    docker run --name jrvs-psql -e POSTGRES_PASSWORD="${db_password}" -e POSTGRES_USER="${db_username}" -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres
-    exit $?
-fi
-
-#if docker container has not been created, error
-if [ "$container_wc" != 2 ]; then
-  echo "Error: Docker container \"jrvs-psql\" does not exist."
-  exit 1
-fi
-
-#if command is start/stop, then start/stop docker container
-case "${cmd}" in
-  "start")
-    docker container start jrvs-psql
-    exit $?
-  ;;
-  "stop")
-    docker container stop jrvs-psql
+    docker container "$cmd" jrvs-psql
     exit $?
   ;;
 esac
