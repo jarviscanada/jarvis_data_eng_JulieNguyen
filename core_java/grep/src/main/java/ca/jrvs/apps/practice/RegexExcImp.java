6458b0bcda260b3cba0ea2c32d4377f76219e279
Julie Nguyen <julie6.nguyen@gmail.com>
2021-09-16 17:12:24 +0000

    java grep implementation updated

---
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexExcImp.java
@@ -0,0 +1,15 @@
+package ca.jrvs.apps.practice;
+
+public class RegexExcImp implements RegexExc {
+
+    public static void main(String[] args){
+
+    }
+
+    public boolean matchJpeg(String filename) {return true;};
+
+    public boolean matchIp(String ip) {return true;};
+
+    public boolean isEmptyLine(String line) {return true;};
+
+}
