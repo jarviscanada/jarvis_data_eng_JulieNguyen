7e3cca0f4bf7f8b34e52e78cd87e6b00d73c901c
Julie Nguyen <julie6.nguyen@gmail.com>
2021-09-17 17:14:43 +0000

    lambdaStreamExc added

--- core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexExcImp.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexExcImp.java
@@ -1,15 +1,24 @@
 package ca.jrvs.apps.practice;
 
+import java.util.regex.*;
+
 public class RegexExcImp implements RegexExc {
 
-    public static void main(String[] args){
+    public static void main(String[] arg ){ };
 
-    }
+    public boolean matchJpeg(String filename) {
+        String regex = ".*\\.jpeg$";
+        return Pattern.matches(regex, filename);
+    };
 
-    public boolean matchJpeg(String filename) {return true;};
+    public boolean matchIp(String ip) {
+        String regex = "([0-9]{1,3}\\.){3}([0-9]{1,3})";
+        return Pattern.matches(regex, ip);
+    };
 
-    public boolean matchIp(String ip) {return true;};
-
-    public boolean isEmptyLine(String line) {return true;};
+    public boolean isEmptyLine(String line) {
+        String regex = "^\\s*$";
+        return Pattern.matches(regex, line);
+    };
 
 }
