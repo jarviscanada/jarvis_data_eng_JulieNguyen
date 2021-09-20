a466f16a83bc5e659ebdd83f162344fac0c49525
Julie Nguyen <julie6.nguyen@gmail.com>
2021-09-20 19:13:06 +0000

    JavaDoc in JavaGrep written

--- core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrep.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrep.java
@@ -4,15 +4,39 @@ import java.io.*;
 import java.util.*;
 
 public interface JavaGrep {
-
+    /**
+     * Top level search workflow
+     * @throws IOException
+     */
     void process() throws IOException;
 
+    /**
+     * Traverse the given rootDir path and return all files in it
+     * @param rootDir
+     * @return
+     */
     List<File> listFiles(String rootDir);
 
+    /**
+     * Reads a file and returns all lines in an ArrayList (BufferedReader)
+     * @param inputFile
+     * @return
+     * @throws IOException
+     */
     List<String> readLines(File inputFile) throws IOException;
 
+    /**
+     * Checks if a line contains the regex pattern given by the user input
+     * @param line
+     * @return
+     */
     boolean containsPattern(String line);
 
+    /**
+     * Writes filtered lines to a file, with the filename given by the user (FileOutputStream)
+     * @param lines
+     * @throws IOException
+     */
     void writeToFile(List<String> lines) throws IOException;
 
     String getRootPath();
