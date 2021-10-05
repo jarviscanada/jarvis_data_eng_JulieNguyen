917e255406093bb07b4227bbe22b48ef151b193c
Julie Nguyen <julie6.nguyen@gmail.com>
2021-10-05 21:21:52 +0000

    service implementation added

---
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/service/TwitterService.java
@@ -0,0 +1,62 @@
+package ca.jrvs.apps.twitter.service;
+
+import ca.jrvs.apps.twitter.dao.CrdDao;
+import ca.jrvs.apps.twitter.model.Coordinates;
+import ca.jrvs.apps.twitter.model.Tweet;
+
+import java.util.List;
+
+public class TwitterService implements Service{
+
+    private CrdDao dao;
+
+    private static final int TWEET_LIMIT = 280;
+    private static final float LONG_MIN = -180;
+    private static final float LONG_MAX = 180;
+    private static final float LAT_MIN = -90;
+    private static final float LAT_MAX = 90;
+
+    public TwitterService(CrdDao dao) {
+        this.dao = dao;
+    }
+
+    @Override
+    public Tweet postTweet(Tweet tweet){
+        //Business logic:
+        //text length, lat/lon range/ id format
+        validatePostTweet(tweet);
+
+        return (Tweet) dao.create(tweet);
+    }
+
+    @Override
+    public Tweet showTweet(String id, String[] fields){
+        return null;
+    }
+
+    @Override
+    public List<Tweet> deleteTweets(String[] ids){
+        return null;
+    }
+
+    public void validateShowTweet(String id){
+        if(!id.matches("[0-9]+")){
+            throw new IllegalArgumentException("ID must be numerical characters.");
+        }
+    }
+
+    public void validatePostTweet(Tweet tweet){
+
+        float [] coordinates = tweet.getCoordinates().getCoordinates();
+
+        if(tweet.getText().length()>=TWEET_LIMIT){
+            throw new IllegalArgumentException("Tweet must be 280 characters or less.");
+        }
+        else if(coordinates[0] > LONG_MAX || coordinates[0] < LONG_MIN ||
+            coordinates[1] > LAT_MAX || coordinates[1] < LAT_MIN){
+            throw new IllegalArgumentException("Coordinates are out of range.");
+        }
+
+    }
+
+}
