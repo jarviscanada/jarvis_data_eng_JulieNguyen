dd32e1b12f387cd63683a2a809edb85900fba0bf
Julie Nguyen <julie6.nguyen@gmail.com>
2021-11-18 21:33:44 +0000

    quotedao finished

--- springboot/src/main/java/ca/jrvs/apps/trading/AppConfig.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/AppConfig.java
@@ -5,6 +5,7 @@ import ca.jrvs.apps.trading.dao.MarketDataDao;
 import ca.jrvs.apps.trading.dao.QuoteDao;
 import ca.jrvs.apps.trading.model.config.MarketDataConfig;
 import ca.jrvs.apps.trading.service.QuoteService;
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.http.conn.HttpClientConnectionManager;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.slf4j.Logger;
@@ -15,11 +16,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 
+import javax.sql.DataSource;
+
 @Configuration
 @ComponentScan("ca.jrvs.apps.trading")
 public class AppConfig {
 
     private Logger logger = LoggerFactory.getLogger(AppConfig.class);
+    private String jdbcUrl, user, password;
+
 
     @Bean
     public MarketDataConfig marketDataConfig(){
@@ -27,6 +32,25 @@ public class AppConfig {
         marketDataConfig.setHost("https://cloud.iexapis.com/v1/");
         marketDataConfig.setToken(System.getenv("IEX_PUB_TOKEN"));
         return new MarketDataConfig();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        jdbcUrl =
+                "jdbc:postgresql://" +
+                        System.getenv("PSQL_HOST") + ":" +
+                        System.getenv("PSQL_PORT") +
+                        "/" +
+                        System.getenv("PSQL_DB");
+        user = System.getenv("PSQL_USER");
+        password = System.getenv("PSQL_PASSWORD");
+
+        //Never log your credentials/secrets. Use IDE debugger instead
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(jdbcUrl);
+        basicDataSource.setUsername(user);
+        basicDataSource.setPassword(password);
+        return basicDataSource;
     }
 
     @Bean
--- springboot/src/main/java/ca/jrvs/apps/trading/dao/QuoteDao.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/dao/QuoteDao.java
@@ -1,13 +1,163 @@
 package ca.jrvs.apps.trading.dao;
 
+import ca.jrvs.apps.trading.model.domain.Quote;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataRetrievalFailureException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
+import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.SqlParameter;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
 @Repository
-public class QuoteDao {
+public class QuoteDao implements CrudRepository<Quote, String> {
+
+    private static final String TABLE_NAME = "quote";
+    private static final String ID_COLUMN_NAME = "ticker";
+
+    private static final Logger logger = LoggerFactory.getLogger(QuoteDao.class);
+    private JdbcTemplate jdbcTemplate;
+    private SimpleJdbcInsert simpleJdbcInsert;
 
     @Autowired
-    public QuoteDao(){
-
+    public QuoteDao(DataSource dataSource){
+        jdbcTemplate = new JdbcTemplate(dataSource);
+        simpleJdbcInsert = new SimpleJdbcInsert(dataSource).withTableName(TABLE_NAME);
     }
+
+    @Override
+    public Quote save(Quote quote) {
+        if(existsById(quote.getTicker())){
+            int updatedRowNo = updateOne(quote);
+            if(updatedRowNo != 1){
+                throw new DataRetrievalFailureException("Unable to update quote");
+            }
+        }
+        else{
+            addOne(quote);
+        }
+        return quote;
+    }
+
+    private void addOne(Quote quote){
+        SqlParameterSource parameterSource = new BeanPropertySqlParameterSource(quote);
+        int row = simpleJdbcInsert.execute(parameterSource);
+        if(row!=1){
+            throw new IncorrectResultSizeDataAccessException("Failed ot insert", 1, row);
+        }
+    }
+
+    private int updateOne(Quote quote){
+        String update_sql = "UPDATE " + TABLE_NAME + " SET last_price=?, bid_price=?, " +
+                "bid_size=?, ask_price=?, ask_size=? WHERE " + ID_COLUMN_NAME + "=?";
+        if(!existsById(quote.getTicker()))
+            throw new NoSuchElementException("Ticker not found: " + quote.getTicker());
+        return jdbcTemplate.update(update_sql, makeUpdateValues(quote));
+    }
+
+    private Object[] makeUpdateValues(Quote quote){
+        Object[] values = {quote.getLastPrice(), quote.getBidPrice(), quote.getBidSize(),
+            quote.getAskPrice(), quote.getAskPrice(), quote.getTicker()};
+        return values;
+    }
+
+    @Override
+    public <S extends Quote> List<S> saveAll(Iterable<S> quotes) {
+        List<Quote> quotesList = new ArrayList<Quote>();
+        for(S quote : quotes){
+            Quote result = save(quote);
+            quotesList.add(result);
+        }
+        return (List<S>) quotesList;
+    }
+
+    @Override
+    public List<Quote> findAll() {
+        String query = "SELECT * FROM " + TABLE_NAME;
+        List<Quote> quotes =  jdbcTemplate
+                .query(query, BeanPropertyRowMapper.newInstance(Quote.class));
+        return quotes;
+    }
+
+    @Override
+    public Optional<Quote> findById(String ticker) {
+        String query = "SELECT * FROM " + TABLE_NAME + "WHERE " + ID_COLUMN_NAME + " = ?";
+        Optional<Quote> quote = null;
+        try{
+             quote = Optional.ofNullable(jdbcTemplate.queryForObject(query,
+                    BeanPropertyRowMapper.newInstance(Quote.class)));
+        }
+        catch(EmptyResultDataAccessException e){
+            logger.debug("Can't find trader id:" + e);
+        }
+        if(quote.isPresent()) {
+            return quote;
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean existsById(String ticker) {
+        String query = "SELECT TOP 1 " + ID_COLUMN_NAME + "FROM " + TABLE_NAME +
+                "WHERE " + ID_COLUMN_NAME + " = ?";
+        Optional<Quote> quote = Optional.ofNullable(jdbcTemplate.queryForObject(query,
+                    Quote.class));
+        if(quote.isPresent()) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void deleteById(String ticker) {
+        if (ticker == null) {
+            throw new IllegalArgumentException("Ticker can't be null");
+        }
+        String query = "DELETE FROM " + TABLE_NAME + " WHERE " + ID_COLUMN_NAME + " =?";
+        jdbcTemplate.update(query, ticker);
+    }
+
+    @Override
+    public long count() {
+        String query = "SELECT COUNT(*) FROM " + TABLE_NAME;
+        return jdbcTemplate.queryForObject(query, Long.class);
+    }
+
+    @Override
+    public void deleteAll() {
+        String query = "DELETE FROM " + TABLE_NAME;
+        jdbcTemplate.update(query);
+    }
+
+    @Override
+    public void delete(Quote quote) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends Quote> iterable) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    @Override
+    public Iterable<Quote> findAllById(Iterable<String> iterable) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+
 }
